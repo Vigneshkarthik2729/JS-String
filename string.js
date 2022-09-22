b213380d708fb9cdff20af50c2a0dda8d69984ca
vignesh <vigneshwaran274mail@gmail.com>
2022-09-22 14:11:44 +0530

    string

---
+++ string.js
@@ -0,0 +1,52 @@
+//to lowercase()
+let lowercase='VIGNESH';
+console.log("Lowercase:",lowercase.toLowerCase());
+
+
+//to uppercase()
+let uppercase='vignesh';
+console.log("uppercase:",uppercase.toUpperCase());
+
+//repeat
+let repeat='hello';
+console.log("Repeat:",repeat.repeat(10));
+
+//replace
+let replace='karthik'
+console.log("Replace:",replace.replace('karthik','vignesh'));
+
+
+//check string ends with
+let endswith='jollyojymkha'
+console.log("EndsWith:",endswith.endsWith('a'));
+
+//includes
+let includes='laptop'
+console.log("Includes:",includes.includes('t'));
+
+//concat
+let user='Hello';
+let user1='Buddy';
+console.log("Concat:",user.concat(user1));
+
+//indexOf
+let users='virat'
+console.log("Index Of:",users.indexOf('a'));
+
+//split
+let a='karthik'
+let result=a.split('')
+console.log("Split:",...result);
+
+//slice startindex endindex
+let html='Hypertext Markup Language'
+console.log("Slice:",html.slice(2,8));
+
+
+//search
+let search='Royal Enfield'
+console.log("Search:",search.search('l'));
+
+// indexOfCharacter
+let fruits='mango'
+console.log("Index of character:",fruits.charAt(3));
